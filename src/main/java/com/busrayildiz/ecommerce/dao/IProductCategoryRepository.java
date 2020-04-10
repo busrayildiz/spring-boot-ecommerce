1d7b51ad4b30b2dcd2717cef1bbfac5280daf053
busrayildiz <bbusrayildizz34@gmail.com>
2020-04-10 17:01:36 +0300

    first commit

--- src/main/java/com/busrayildiz/ecommerce/dao/IProductCategoryRepository.java
+++ src/main/java/com/busrayildiz/ecommerce/dao/IProductCategoryRepository.java
@@ -3,9 +3,11 @@ package com.busrayildiz.ecommerce.dao;
 import com.busrayildiz.ecommerce.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-                                                //nameOfJSONEntry
+//nameOfJSONEntry
 @RepositoryRestResource(collectionResourceRel = "productCategory" , path = "product-category")
+@CrossOrigin("http://localhost:4200")
 public interface IProductCategoryRepository extends JpaRepository<ProductCategory,Long>
 {
 
